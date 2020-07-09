51dc6d0ea936f7580c4c98f8790f41017b7a37e2
Sylvain Rousson <sylvain@diabeloop.fr>
2020-07-09 18:53:21 +0200

    First commit
    android app skeleon created

---
+++ app/src/main/java/com/sylvainrousson/linkoffee/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sylvainrousson.linkoffee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sylvainrousson.linkoffee.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, MainFragment.newInstance()).commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/com/sylvainrousson/linkoffee/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.sylvainrousson.linkoffee.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
